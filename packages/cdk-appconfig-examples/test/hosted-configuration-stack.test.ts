24c9a9ee0b9e0841f7c5759f611517469b3543c0
Jeff Cooper <jeff@cuperman.net>
2021-05-22 10:06:01 -0700

    Create example of hosted config with deployment

--- packages/cdk-appconfig-examples/test/hosted-configuration-stack.test.ts
+++ packages/cdk-appconfig-examples/test/hosted-configuration-stack.test.ts
@@ -1,10 +1,10 @@
 import { expect as expectCDK, matchTemplate, MatchStyle, haveResource } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
-import * as CdkExamples from '../lib/cdk-examples-stack';
+import { HostedConfigurationStack } from '../lib';
 
 describe('CdkExamplesStack', () => {
   const app = new cdk.App();
-  const stack = new CdkExamples.CdkExamplesStack(app, 'MyTestStack');
+  const stack = new HostedConfigurationStack(app, 'MyTestStack');
 
   it('has an AppConfig Application', () => {
     expectCDK(stack).to(haveResource('AWS::AppConfig::Application'));
