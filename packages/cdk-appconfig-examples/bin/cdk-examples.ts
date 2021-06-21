ba93f0a67f2b45027d5c808dcd1182a93700b6d6
Jeff Cooper <jeff@cuperman.net>
2021-06-21 06:04:48 -0700

    Example s3 configuration stack

--- packages/cdk-appconfig-examples/bin/cdk-examples.ts
+++ packages/cdk-appconfig-examples/bin/cdk-examples.ts
@@ -1,10 +1,12 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { HostedConfigurationStack, LambdaExtensionStack } from '../lib';
+import { HostedConfigurationStack, LambdaExtensionStack, S3ConfigurationStack } from '../lib';
 
 const app = new cdk.App();
 
 new HostedConfigurationStack(app, 'AppConfigExample-HostedConfiguration');
 
 new LambdaExtensionStack(app, 'AppConfigExample-LambdaExtension');
+
+new S3ConfigurationStack(app, 'AppConfigExample-S3Configuration');
--- packages/cdk-appconfig-examples/lib/index.ts
+++ packages/cdk-appconfig-examples/lib/index.ts
@@ -1,2 +1,3 @@
 export * from './hosted-configuration-stack';
 export * from './lambda-extension-stack';
+export * from './s3-configuration-stack';
